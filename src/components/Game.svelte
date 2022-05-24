7dffb8638f0b442e59f75a2a6928f48fc0397410
Fatih20 <fnri39@protonmail.com>
2022-05-24 10:03:06 +0700

    Moving the head coordinate and initiate keydown response.

--- src/components/Game.svelte
+++ src/components/Game.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
-  import { gridSize } from "../config";
-  import type {
+  import { xlink_attr } from "svelte/internal";
+
+  import { gridSize, refreshTime } from "../config";
+  import {
     cellCoordinate,
+    directionVectorType,
     makePossibleCoordinate,
     possibleCoordinateType,
   } from "../utilities/types";
@@ -15,15 +18,52 @@
 
   const gridRowColumnString = `repeat(${gridSize}, ${cellSize})`;
 
-  const arrayOfCoordinate: cellCoordinate[] = Array.from(
-    { length: 1 },
-    (_, i) => randomCoordinate()
-  );
+  // const arrayOfCoordinate: cellCoordinate[] = Array.from(
+  //   { length: 1 },
+  //   (_, i) => randomCoordinate()
+  // );
+
+  let headCoordinate: cellCoordinate = randomCoordinate();
 
   let direction = randomDirection();
-  $: directionVectorValue = directionsProperty[direction].vectorValue;
-  console.log(arrayOfCoordinate);
+  $: directionVector = directionsProperty[direction].vectorValue;
+
+  function mover(
+    movedCoordinate: cellCoordinate,
+    directionVector: directionVectorType
+  ) {
+    let newX: number;
+    let newY: number;
+    const { x: incrementX, y: incrementY } = directionVector;
+    if (movedCoordinate.x + incrementX > gridSize) {
+      newX = 1;
+    } else if (movedCoordinate.x + incrementX < 1) {
+      newX = gridSize;
+    } else {
+      newX = movedCoordinate.x + incrementX;
+    }
+
+    if (movedCoordinate.y + incrementY > gridSize) {
+      newY = 1;
+    } else if (movedCoordinate.y + incrementY < 1) {
+      newY = gridSize;
+    } else {
+      newY = movedCoordinate.y + incrementY;
+    }
+
+    return { x: makePossibleCoordinate(newX), y: makePossibleCoordinate(newY) };
+  }
+
+  function handleKeydown(e) {
+    console.log(e);
+  }
+
+  setInterval(() => {
+    headCoordinate = mover(headCoordinate, directionVector);
+  }, refreshTime);
 </script>
+
+<svelte:window on:keydown={handleKeydown} />
 
 <main>
   <div
@@ -31,13 +71,19 @@
     style:grid-template-rows={gridRowColumnString}
     style:grid-template-columns={gridRowColumnString}
   >
-    {#each arrayOfCoordinate as coordinate}
+    <div
+      class="cell"
+      style:grid-column={`${headCoordinate.x}/${headCoordinate.x + 1}`}
+      style:grid-row={`${headCoordinate.y}/${headCoordinate.y + 1}`}
+    />
+
+    <!-- {#each arrayOfCoordinate as coordinate}
       <div
         class="cell"
         style:grid-column={`${coordinate.x}/${coordinate.x + 1}`}
         style:grid-row={`${coordinate.y}/${coordinate.y + 1}`}
       />
-    {/each}
+    {/each} -->
   </div>
 </main>
 
--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,2 @@
 export const gridSize = 10;
+export const refreshTime = 1000;
