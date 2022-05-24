76d97de0a37a8f494c3809f631ad45e370443344
Fatih20 <fnri39@protonmail.com>
2022-05-24 07:14:11 +0700

    Lay the components out in app.

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,20 @@
 <script lang="ts">
-	export let name: string;
+  import Header from "./components/Header.svelte";
+  import Game from "./components/Game.svelte";
+  import Footer from "./components/Footer.svelte";
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <Header />
+  <Game />
+  <Footer />
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  main {
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
 </style>
