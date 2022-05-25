ae1e63b0448e9b7eaaf1eacc4d3a24a20bc6a865
Fatih20 <fnri39@protonmail.com>
2022-05-25 14:16:48 +0700

    Better footer layout.

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -2,10 +2,18 @@
 </script>
 
 <main>
-  <p>Copyright &copy; Fatih Nararya R. I. | All Rights Reserved</p>
+  <p>Copyright &copy; Fatih Nararya R. I.</p>
+  <p>All Rights Reserved</p>
 </main>
 
 <style>
+  main {
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 0.25em;
+  }
   p {
     text-align: center;
   }
