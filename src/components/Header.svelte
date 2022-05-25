e252b229bb0cc4651d90e7377b335ec1ae89c0c1
Fatih20 <fnri39@protonmail.com>
2022-05-25 14:35:26 +0700

    Initial work on header.

---
+++ src/components/Header.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+</script>
+
+<main>
+  <h1>Snake Classics</h1>
+</main>
+
+<style>
+  main {
+    align-items: center;
+    background-color: #206b00;
+    color: white;
+    display: flex;
+    justify-content: center;
+    width: 100%;
+  }
+</style>
