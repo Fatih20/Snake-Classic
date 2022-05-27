abfe66fb607950a413e4acdf55679e908c3e1ef1
Fatih20 <fnri39@protonmail.com>
2022-05-27 09:45:04 +0700

    Styling of main menu complete.

--- src/components/StartPage.svelte
+++ src/components/StartPage.svelte
@@ -8,22 +8,98 @@
   }
 </script>
 
+<head>
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap"
+    rel="stylesheet"
+  />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Asap:wght@400;500;600;700&display=swap"
+    rel="stylesheet"
+  />
+</head>
+
 <main>
-  <h1 id="title">Snake Classics</h1>
-  <button on:click={() => sendGameHasStarted()}>Start</button>
+  <div id="content-container">
+    <h1 id="title">Snake<br />Classics</h1>
+    <div class="spacer" />
+    <button
+      class="start-button"
+      on:click={() => {
+        setTimeout(() => {
+          sendGameHasStarted();
+        }, 250);
+      }}>Start</button
+    >
+  </div>
 </main>
 
 <style>
   main {
     align-items: center;
+    color: var(--text-on-primary-element-color);
+    background-color: var(--primary-color);
     display: flex;
     flex-direction: column;
     justify-content: center;
-    gap: 1em;
+    height: 100vh;
+    width: 100%;
+  }
+
+  main * {
+    color: var(--text-on-primary-element-color);
+  }
+
+  #content-container {
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 1.5em;
+    height: 300px;
+    /* border: solid 1px white; */
   }
 
   #title {
+    font-family: "Pacifico", cursive;
     text-align: center;
     font-size: 4em;
+    font-weight: 400;
+    line-height: 1.25em;
+  }
+
+  .spacer {
+    flex-grow: 1;
+  }
+
+  .start-button {
+    background-color: var(--primary-color);
+    /* border: solid 2px black; */
+    border: none;
+    /* box-sizing: border-box; */
+    border-radius: var(--button-radius);
+    /* box-shadow: 0 5px 10px rgba(0, 0, 0, 0.4); */
+    cursor: pointer;
+    font-family: "Asap", sans-serif;
+    font-size: 1.75em;
+    font-weight: 700;
+    padding: 0.25em 0;
+    transition: all 0.1s ease-in-out;
+    width: 100%;
+  }
+
+  .start-button:hover {
+    background-color: var(--secondary-color);
+    color: var(--text-on-secondary-selected-element-color);
+    /* font-size: 2em; */
+    /* padding: 0.4em 0; */
+  }
+
+  .clicked {
+    background-color: var(--secondary-active-color) !important;
+    color: var(--text-on-secondary-active-element-color) !important;
+    transition: all 0.25s ease-in-out !important;
   }
 </style>
