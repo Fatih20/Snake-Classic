e2f31e0376d5f21e052a5b745ad4f3bf27424379
Fatih20 <fnri39@protonmail.com>
2022-05-27 10:28:22 +0700

    Header button and title is centered.

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -9,21 +9,24 @@
 </head>
 
 <main>
-  <button
-    on:click={() =>
-      gameIsPaused.update((gameIsPaused) => (gameIsPaused = !gameIsPaused))}
-    class="header-button"
-  >
-    {#if $gameIsPaused}
-      <i class="fa-solid fa-play icon" />
-    {:else}
-      <i class="fa-solid fa-pause icon" />
-    {/if}
-  </button>
-  <h1>Snake Classics</h1>
-  <button class="header-button">
-    <i class="fa-solid fa-right-to-bracket icon" />
-  </button>
+  <div class="width-constrain">
+    <!-- <button>Bruh</button> -->
+    <button
+      on:click={() =>
+        gameIsPaused.update((gameIsPaused) => (gameIsPaused = !gameIsPaused))}
+      class="header-button"
+    >
+      {#if $gameIsPaused}
+        <i class="fa-solid fa-play icon" />
+      {:else}
+        <i class="fa-solid fa-pause icon" />
+      {/if}
+    </button>
+    <h1>Snake Classics</h1>
+    <button class="header-button">
+      <i class="fa-solid fa-right-to-bracket icon" />
+    </button>
+  </div>
 </main>
 
 <style>
@@ -35,9 +38,27 @@
     color: var(--text-on-primary-element-color);
     display: flex;
     /* height: var(--header-height); */
-    justify-content: space-between;
+    justify-content: center;
     padding: 0.5em 1em;
     width: 100%;
+  }
+
+  main * {
+    color: var(--text-on-primary-element-color);
+  }
+
+  .width-constrain {
+    align-items: center;
+    box-sizing: border-box;
+    display: flex;
+    justify-content: space-between;
+    width: min(100%, 400px);
+
+    /* border: solid 1px white; */
+  }
+
+  .width-constrain > * {
+    /* border: solid 1px white; */
   }
 
   h1 {
@@ -46,18 +67,21 @@
   }
 
   .header-button {
+    align-self: center;
     background-color: rgba(0, 0, 0, 0);
     border: none;
     color: white;
     cursor: pointer;
     display: flex;
     font-size: var(--font-size);
+    margin: 0;
     padding: 0;
 
     /* border: solid 1px white; */
   }
 
   .icon {
+    display: block;
     /* border: solid 1px white; */
   }
 </style>
