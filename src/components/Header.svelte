cee54e8a798732ee6f235e20fea3522b6ff596fe
Fatih20 <fnri39@protonmail.com>
2022-05-29 18:58:22 +0700

    Attempts at bringing back the fontawesome icon.

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -6,6 +6,14 @@
   <script
     src="https://kit.fontawesome.com/31a5898fa1.js"
     crossorigin="anonymous"></script>
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css"
+    integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA=="
+    crossorigin="anonymous"
+    referrerpolicy="no-referrer"
+  />
+
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" />
   <link
