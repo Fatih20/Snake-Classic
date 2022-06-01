3065b4bb4f002439e8d7e1531fea7716c0b487a9
Fatih20 <fnri39@protonmail.com>
2022-06-01 22:34:43 +0700

    Initial work on styling the login and signin.

--- src/components/login.svelte
+++ src/components/login.svelte
@@ -11,7 +11,9 @@
 </script>
 
 <main>
-  <h2>{$gameState === "login" ? "Log In" : "Sign In"}</h2>
+  <h2 class="title">
+    {$gameState === "login" ? "Log In" : "Sign In"}
+  </h2>
   <form on:submit={handleSubmit} method="POST">
     <div class="input-element">
       <label for="username-input">Username</label>
@@ -44,20 +46,20 @@
       />
     </div>
   </form>
-  <button type="submit">{$gameState === "login" ? "Log In" : "Sign In"}</button>
-  {#if $gameState === "login"}
-    <p>
+  <button class="submit-button" type="submit"
+    >{$gameState === "login" ? "Log In" : "Sign In"}</button
+  >
+  <p class="alternate-text">
+    {#if $gameState === "login"}
       Don't have an account? <span on:click={() => gameState.set("signIn")}
         >Sign in.</span
       >
-    </p>
-  {:else if $gameState === "signIn"}
-    <p>
+    {:else if $gameState === "signIn"}
       Already have an account? <span on:click={() => gameState.set("login")}
         >Log in.</span
       >
-    </p>
-  {/if}
+    {/if}
+  </p>
 </main>
 
 <style>
@@ -66,16 +68,71 @@
     display: flex;
     flex-direction: column;
     justify-content: center;
+    gap: 1em;
   }
 
-  button {
+  .title,
+  label,
+  span {
+    color: rgb(var(--primary-color));
+  }
+
+  form {
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 0.5em;
+  }
+
+  label {
+    font-weight: 500;
+  }
+
+  input {
+    box-sizing: border-box;
+    background-color: rgb(var(--primary-color));
+    border: solid 2px rgba(var(--primary-color), 0);
+    border-radius: var(--button-radius);
+    color: rgb(var(--text-on-primary-element-color));
+    outline-color: rgb(var(--text-on-primary-element-color));
+    outline-style: solid;
+    outline-width: 2px;
+    width: 100%;
+  }
+
+  input:focus {
+    background-color: rgb(var(--text-on-primary-element-color));
+    border: solid 2px rgb(var(--primary-color));
+    color: rgb(var(--primary-color));
+  }
+
+  ::placeholder {
+    color: rgba(var(--text-on-primary-element-color), 0.75);
+  }
+
+  .submit-button {
+    background-color: rgb(var(--primary-color));
+    border: none;
+    border-radius: var(--button-smaller-radius);
+    color: rgb(var(--text-on-primary-element-color));
+    font-weight: 600;
     margin: 0;
   }
 
   .input-element {
+    align-items: flex-start;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 0.5em;
   }
 
-  input {
-    width: 100%;
+  .alternate-text {
+    font-weight: 500;
+  }
+
+  span {
+    cursor: pointer;
   }
 </style>
