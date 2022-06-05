4c699a1aa2228d23ee15b064b92f8a070606b892
Fatih20 <fnri39@protonmail.com>
2022-06-05 21:37:09 +0700

    Rename login page file name to camel case.

---
+++ src/components/Login.svelte
@@ -0,0 +1,149 @@
+<script lang="ts">
+  import {
+    placeholderEmail,
+    placeholderPassword,
+    placeholderUsername,
+  } from "../config";
+
+  import { gameState } from "../stores";
+
+  let enteredEmail = "";
+  let enteredPassword = "";
+  let enteredUsername = "";
+
+  function handleSubmit(e) {}
+</script>
+
+<main>
+  <h2 class="title">
+    {$gameState === "login" ? "Log In" : "Sign In"}
+  </h2>
+  <form on:submit={handleSubmit} method="POST">
+    <div class="input-element">
+      <label for="username-input">Username</label>
+      <input
+        id="username-input"
+        name="username"
+        placeholder={placeholderUsername}
+        bind:value={enteredUsername}
+      />
+    </div>
+    {#if $gameState === "signIn"}
+      <div class="input-element">
+        <label for="email-input">Email</label>
+        <input
+          id="email-input"
+          name="email"
+          type="email"
+          placeholder={placeholderEmail}
+          bind:value={enteredEmail}
+        />
+      </div>
+    {/if}
+    <div class="input-element">
+      <label for="password-input">Password</label>
+      <input
+        type="password"
+        id="password-input"
+        name="password"
+        placeholder={placeholderPassword}
+        bind:value={enteredPassword}
+      />
+    </div>
+  </form>
+  <button class="submit-button" type="submit"
+    >{$gameState === "login" ? "Log In" : "Sign In"}</button
+  >
+  <p class="alternate-text">
+    {#if $gameState === "login"}
+      Don't have an account? <span on:click={() => gameState.set("signIn")}
+        >Sign in.</span
+      >
+    {:else if $gameState === "signIn"}
+      Already have an account? <span on:click={() => gameState.set("login")}
+        >Log in.</span
+      >
+    {/if}
+  </p>
+</main>
+
+<style>
+  main {
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 1em;
+  }
+
+  .title,
+  label,
+  span {
+    color: rgb(var(--primary-color));
+  }
+
+  form {
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 0.5em;
+  }
+
+  label {
+    font-weight: 500;
+  }
+
+  input {
+    box-sizing: border-box;
+    background-color: rgb(var(--primary-color));
+    border: solid 2px rgba(var(--primary-color), 0);
+    border-radius: var(--button-radius);
+    color: rgb(var(--text-on-primary-element-color));
+    outline: none;
+    width: 100%;
+  }
+
+  input:focus {
+    border: solid 2px rgba(var(--primary-border-color), 1);
+  }
+
+  ::placeholder {
+    color: rgba(var(--text-on-primary-element-color), 0.75);
+  }
+
+  .submit-button {
+    background-color: rgb(var(--primary-color));
+    border: solid 2px rgba(var(--primary-color), 0);
+    border-radius: var(--button-smaller-radius);
+    color: rgb(var(--text-on-primary-element-color));
+    cursor: pointer;
+    font-weight: 600;
+    transition: all 0.25s ease-in-out;
+    /* font-size: 1em; */
+    margin: 0;
+    padding: 7px;
+  }
+
+  .submit-button:hover {
+    background-color: rgb(var(--text-on-primary-element-color));
+    border: solid 2px rgba(var(--primary-color), 1);
+    color: rgb(var(--primary-color));
+  }
+
+  .input-element {
+    align-items: flex-start;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 0.5em;
+  }
+
+  .alternate-text {
+    font-weight: 500;
+  }
+
+  span {
+    cursor: pointer;
+  }
+</style>
