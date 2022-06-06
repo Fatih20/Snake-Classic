54706d14cd7276340b452c7d9d170043e40cce6b
Fatih20 <fnri39@protonmail.com>
2022-06-06 13:03:25 +0700

    Attempts at simplifying position relative to.

--- src/utilities/utilitiesCoreGame.ts
+++ src/utilities/utilitiesCoreGame.ts
@@ -75,28 +75,25 @@ export function positionRelativeTo (coordinate1 : cellCoordinate, coordinate2 : 
 
     const incrementX = x2 - x1;
     const incrementY = y2 - y1;
+    let directionVectorOneToTwo : directionVectorType;
     // console.log(Math.abs(incrementX) / incrementX);
     // console.log(incrementY);
     // console.log(Math.abs(incrementY) / incrementY);
-    let directionVectorOneToTwo : directionVectorType = {
-        x : makePossibleVectorValue(incrementX !== 0 ? Math.abs(incrementX) / incrementX : 0),
-        y : makePossibleVectorValue(incrementY !== 0 ? Math.abs(incrementY) / incrementY : 0)
-    };
-    // if (Math.abs(incrementX) > 1) {
-    //     if (incrementX < 0) {
-    //         directionVectorOneToTwo = {x : makePossibleVectorValue(1), y : makePossibleVectorValue(0)}    
-    //     } else {
-    //         directionVectorOneToTwo = {x : makePossibleVectorValue(-1), y : makePossibleVectorValue(0)}
-    //     }
-    // } else if (Math.abs(incrementY) > 1) {
-    //     if (incrementY < 0) {
-    //         directionVectorOneToTwo = {x : makePossibleVectorValue(0), y : makePossibleVectorValue(1)}    
-    //     } else {
-    //         directionVectorOneToTwo = {x : makePossibleVectorValue(0), y : makePossibleVectorValue(-1)}
-    //     }
-    // } else {
-    //     directionVectorOneToTwo ={x : makePossibleVectorValue(incrementX), y : makePossibleVectorValue(incrementY)};
-    // }
+    // let directionVectorOneToTwo : directionVectorType = {
+    //     x : makePossibleVectorValue(incrementX !== 0 ? Math.abs(incrementX) / incrementX : 0),
+    //     y : makePossibleVectorValue(incrementY !== 0 ? Math.abs(incrementY) / incrementY : 0)
+    // };
+    
+    if (incrementX < -1) {
+        directionVectorOneToTwo = {x : makePossibleVectorValue(1), y : makePossibleVectorValue(0)}    
+    } else if (incrementX > 1) {
+        directionVectorOneToTwo = {x : makePossibleVectorValue(-1), y : makePossibleVectorValue(0)}
+    } else if (incrementY < -1) {
+        directionVectorOneToTwo = {x : makePossibleVectorValue(0), y : makePossibleVectorValue(1)}    
+    } else if (incrementY > 1) {
+        directionVectorOneToTwo = {x : makePossibleVectorValue(0), y : makePossibleVectorValue(-1)}
+    } else {
+    directionVectorOneToTwo = {x : makePossibleVectorValue(incrementX), y : makePossibleVectorValue(incrementY)}}
     // const result = Object.keys(directionsProperty).filter((directionName : direction) => {
     //     console.log(directionVectorOneToTwo);
     //     console.log(directionsProperty[directionName].vectorValue);
@@ -167,6 +164,8 @@ export function cornerOfSnakeBodyGenerator(wholeSnakeCoordinateList: cellCoordin
           snakeCoordinate
         );
 
+        console.log(directionFromNextCoordinate);
+
         return `${directionFromNextCoordinate.toLowerCase()}`;
       } else {
         const previousCell = wholeSnakeCoordinateList[index - 1];
@@ -179,6 +178,7 @@ export function cornerOfSnakeBodyGenerator(wholeSnakeCoordinateList: cellCoordin
           nextCell,
           snakeCoordinate
         ) as direction;
+        console.log([directionFromPreviousCell, directionFromNextCell]);
         if (
           oppositeDirectionDictionary[directionFromPreviousCell] ===
           directionFromNextCell
