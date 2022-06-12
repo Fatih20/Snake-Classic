eeab01a5f753f16befcd028ee23b49927898b211
Fatih20 <fnri39@protonmail.com>
2022-06-12 17:00:36 +0700

    Shoot to production.

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export const shootToProduction = false;
+export const shootToProduction = true;
 export const isInProduction = false;
 export const gridSize = 20;
 export const initialRefreshTime = 750;
